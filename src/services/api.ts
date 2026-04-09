41b3ec3d755cd8d6ed6941ffd4ed2450ea5f8cbf
Rajiv Ramesh <rajivramesh92@gmail.com>
2026-04-09 14:25:00 +0530

    hardcode api url fix

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "https://i-salary-management.onrender.com/api/v1",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   timeout: 15000,
   headers: {
     'Content-Type': 'application/json',
